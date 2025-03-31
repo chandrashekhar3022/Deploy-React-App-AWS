aa7b27e58bcafddde153ef8bdae38d23ebbaf9bc
chandrashekhar3022 <chandrashekhar.joshi@reliqus.com>
2025-03-31 15:33:36 +0530

    check the count

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div>App...</div>
-  )
+    <div>
+      <h1>Vite + React</h1>
+      <button>{count}</button>
+
+      <button onClick={() => setCount(count + 1)}>Count+</button>
+      <button onClick={() => setCount(count - 1)} disabled={count <= 0}>
+        Count-
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
